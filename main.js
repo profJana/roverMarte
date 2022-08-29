3b39561bdaac88e90168c97762a74c01e8705371
profJana <112178884+profJana@users.noreply.github.com>
2022-08-29 09:56:00 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,106 @@
+canvas = document.getElementById('myCanvas'); 
+ctx = canvas.getContext("2d");
+
+roverWidth = 100;
+roverHeight = 90;
+
+backgroundImage = "mars.jpg";
+
+roverImage = "rover.png";
+
+roverX = 10;
+roverY = 10;
+
+function add() {
+	backgroundImgTag = new Image(); //definindo uma variável com uma nova imagem
+	backgroundImgTag.onload = uploadBackground; // ajustando uma função, ao carregar essa variável
+	backgroundImgTag.src = backgroundImage;   // carregar a imagem
+
+	roverImgTag = new Image(); //definindo uma variável com uma nova imagem
+	roverImgTag.onload = uploadrover; // ajustando uma função, ao carregar essa variável
+	roverImgTag.src = roverImage;   // carregar a imagem
+}
+
+function uploadBackground() {
+	ctx.drawImage(backgroundImgTag, 0, 0, canvas.width, canvas.height);
+}
+
+function uploadrover() {
+	ctx.drawImage(roverImgTag, roverX, roverY, roverWidth, roverHeight);
+}
+
+
+window.addEventListener("keydown", myKeyDown);
+
+function myKeyDown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up(); //função up que será criada mais em baixo
+			console.log("cima");
+		}
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("baixo");
+		}
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("esquerda");
+		}
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("direita");
+		}
+}
+
+
+
+
+
+
+function up()
+{
+	if(roverY >=0)
+	{
+		roverY = roverY - 10;
+		 uploadBackground();
+		 uploadrover();
+	}
+}
+
+function down()
+{
+	if(roverY <=500)
+	{
+		roverY =roverY+ 10;
+		uploadBackground();
+		 uploadrover();
+	}
+}
+
+function left()
+{
+	if(roverX >= 0)
+	{
+		roverX =roverX - 10;
+		uploadBackground();
+		 uploadrover();
+	}
+}
+
+function right()
+{
+	if(roverX <= 700)
+	{
+		roverX =roverX + 10;
+		uploadBackground();
+		uploadrover();
+   }
+}
+
+	
